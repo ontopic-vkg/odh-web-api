001487ccf567c2685ad6cb02157375354034eb53
skomlaebri <sarahkeb89@gmail.com>
2020-07-23 16:48:01 +0200

    construct query for accommodation

--- src/main/java/io/vertx/web/api/HttpServerVerticle.java
+++ src/main/java/io/vertx/web/api/HttpServerVerticle.java
@@ -2,11 +2,11 @@ package io.vertx.web.api;
 
 import com.apicatalog.jsonld.JsonLd;
 import com.apicatalog.jsonld.api.JsonLdError;
-import com.apicatalog.jsonld.document.Document;
+import com.apicatalog.jsonld.api.impl.FramingApi;
 import com.apicatalog.jsonld.document.JsonDocument;
-import com.apicatalog.jsonld.document.RdfDocument;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+
+import com.google.gson.JsonParser;
+import com.moandjiezana.toml.Toml;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
@@ -18,20 +18,19 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
-import net.minidev.json.JSONObject;
+import org.apache.jena.query.ParameterizedSparqlString;
 
-import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class HttpServerVerticle extends AbstractVerticle {
 
     String jsonPath = "/api/JsonLD/DetailInLD";
     String host = "localhost";
+
     int port = 9090;
 
     public enum Type {
@@ -45,6 +44,7 @@ public class HttpServerVerticle extends AbstractVerticle {
 
         System.out.println("Vert.x instance: " + vertx);
 
+
         Router router = Router.router(vertx);
 
         HttpServerOptions options = new HttpServerOptions()
@@ -53,7 +53,7 @@ public class HttpServerVerticle extends AbstractVerticle {
 
         io.vertx.core.http.HttpServer server = vertx.createHttpServer(options);
 
-        // Test route
+        // TODO remove Test route
         router.route("/").handler(routingContext -> {
 
             HttpServerResponse response = routingContext.response();
@@ -82,49 +82,96 @@ public class HttpServerVerticle extends AbstractVerticle {
 
             HttpServerRequest request = routingContext.request();
 
-            // Test
+            // TODO remove Test
             System.out.println( "URI: " + request.uri() );
             System.out.println( "METHOD: " + request.method() );
             System.out.println( "VERSION: " + request.version() );
             System.out.println( "PATH: " + request.path() );
             System.out.println( "QUERY: " + request.query() );
 
+            HttpServerResponse response = routingContext.response();
+
+            //Read input files
+            InputStream tomlFile = getClass().getClassLoader().getResourceAsStream("construct-queries.toml");
+            InputStream schema = getClass().getClassLoader().getResourceAsStream("schema-context.json");
+            InputStream jsonFramesStream = getClass().getClassLoader().getResourceAsStream("frames.json");
+
+            Toml toml = new Toml().read(tomlFile);
+
+            if (toml == null) {
+                response.setStatusCode(500).end("Invalid toml file");
+                return;
+            }
+
+            if (jsonFramesStream == null) {
+                response.setStatusCode(500).end("Invalid json file");
+                return;
+            }
+            Reader reader = new InputStreamReader(jsonFramesStream);
+            com.google.gson.JsonObject frameObject = new JsonParser().parse(reader).getAsJsonObject();
+
+
+            //TODO: only for testing phase, remove endpoint
+            String endpointHost = request.getParam("endpoint") ;
+
+            if (endpointHost == null) {
+                endpointHost = "sparql.opendatahub.bz.it";
+            }
+            int endpointPort = endpointHost.equals("localhost")? 8080: 80;
+
             // Get query parameters
             String type = request.getParam("type");
             String id = request.getParam("Id");
-
-            String endpoint = request.getParam("endpoint") ;
-            if (endpoint == null || endpoint.isEmpty()) {
-                endpoint = "sparql.opendatahub.bz.it";
-            }
             String language = request.getParam("language") ;
-            if (language == null || language.isEmpty()) {
-                language = "en";
-            }
+            String idtoshow = request.getParam("idtoshow") ;
+            String urltoshow = request.getParam("urltoshow") ;
+            String imageurltoshow = request.getParam("imageurltoshow") ;
+            String showId = request.getParam("showid");
 
-            String showid = request.getParam("showid");
-            if (showid == null || showid.isEmpty()) {
-                showid = "true";
-            }
-
-            HttpServerResponse response = routingContext.response();
-            if(type == null || id == null ){
+            if (type == null || id == null ){
                 response.setStatusCode(400).end("The request" + request.uri() + " is invalid.");
             }
+            if (urltoshow != null && !isValidURI(urltoshow)) {
+                response.setStatusCode(400).end("Error: Invalid URI: The format of the URI could not be determined.");
+            }
+            if (imageurltoshow != null && !isValidURI(imageurltoshow)) {
+                response.setStatusCode(400).end("Error: Invalid URI: The format of the URI could not be determined.");
+            }
+
+
 
             WebClient client = WebClient.create(vertx);
 
             try {
-                String sparqlQuery = getConstructSparqlQuery(type, language, id);
+                String query = getConstructSparqlQuery(toml, type);
 
-                String frame = getFrame(type);
+                ParameterizedSparqlString pss = new ParameterizedSparqlString(query);
+                pss.setLiteral("Id", id);
+                if (language != null){
+                    pss.setLiteral("language", language);
+                }
+                if (idtoshow != null){
+                    pss.setIri("idtoshow", idtoshow);
+                }
+                if (urltoshow != null){
+                    pss.setIri("urltoshow", urltoshow);
+                }
+                if (imageurltoshow != null){
+                    pss.setIri("imageurltoshow", imageurltoshow);
+                }
+                if (showId != null){
+                    pss.setLiteral("showid", showId);
+                }
 
-                boolean showIdBoolean = Boolean.parseBoolean(showid);
 
-//                Buffer buffer = Buffer.buffer("query="+sparqlQuery);
+                String sparqlQuery = pss.toString();
+                System.out.println(sparqlQuery);
+
+                String frame = getFrame(frameObject, type);
+
                 // Send a GET request
                 client
-                        .get(8080, endpoint, "/sparql")
+                        .get(endpointPort, endpointHost, "/sparql")
                         .addQueryParam("query", sparqlQuery)
                         .putHeader("Accept", "application/ld+json")
                         .send( ar -> {
@@ -133,32 +180,33 @@ public class HttpServerVerticle extends AbstractVerticle {
                                 HttpResponse<Buffer> res = ar.result();
                                 String result = res.bodyAsString();
 
-//                                result ="[{\"@id\":\"http://noi.example.org/ontology/odh#data/accommodation/70043B17DAE33F1EFCDA24D4BB4C1F72\",\"@type\":[\"http://schema.org/LodgingBusiness\"],\"http://schema.org/name\":[{\"@value\":\"Ütia Gardenacia\"}],\"http://schema.org/address\":[{\"@id\":\"http://noi.example.org/ontology/odh#data/address/accommodation/70043B17DAE33F1EFCDA24D4BB4C1F72\"}]},{\"@id\":\"http://noi.example.org/ontology/odh#data/address/accommodation/70043B17DAE33F1EFCDA24D4BB4C1F72\",\"@type\":[\"http://schema.org/PostalAddress\"]},{\"@id\":\"http://schema.org/LodgingBusiness\"},{\"@id\":\"http://schema.org/PostalAddress\"}]";
-
                                 System.out.println("Received response with status code " + res.statusCode());
-                                System.out.println(result);
+                                System.out.println("return jsonld " + result);
 
                                 InputStream streamJsonLd =  new ByteArrayInputStream(result.getBytes(StandardCharsets.UTF_8));
 
                                 InputStream streamFrame =  new ByteArrayInputStream(frame.getBytes(StandardCharsets.UTF_8));
 
+
                                 String responseJson = "";
                                 try {
                                     JsonDocument jsonldDoc = JsonDocument.of(streamJsonLd);
                                     JsonDocument frameDoc = JsonDocument.of(streamFrame);
-                                    JsonObject framed = JsonLd.frame(jsonldDoc, frameDoc).get();
+                                    FramingApi framedObject = JsonLd.frame(jsonldDoc, frameDoc);
+                                    if(schema != null) {
+                                        JsonDocument streamDoc = JsonDocument.of(schema);
+                                        framedObject.context(streamDoc);
+                                    };
 
-                                    if (!showIdBoolean) {
-                                        DocumentContext jsonContext = JsonPath.parse(framed.toString());
-                                        jsonContext.delete("$..id");
-                                        responseJson = jsonContext.jsonString();
-                                    }
+                                    JsonObject framed = framedObject.get();
+                                    responseJson = framed.toString();
 
                                     response.putHeader("content-type", "application/ld+json");
 
                                     response.end(responseJson);
                                 } catch (JsonLdError jsonLdError) {
                                     jsonLdError.printStackTrace();
+                                    response.setStatusCode(500).end("Problem processing in JsonLd " + jsonLdError.getMessage());
                                 }
 
 
@@ -176,18 +224,23 @@ public class HttpServerVerticle extends AbstractVerticle {
         };
     }
 
-    private String getFrame(String type) throws Exception {
+    private boolean isValidURI(String input) {
+
+        try {
+           URL obj = new URL(input);
+           obj.toURI();
+           return true;
+        } catch (Exception e1) {
+            return false;
+        }
+    }
+
+    private String getFrame(com.google.gson.JsonObject frameObject, String type) throws Exception {
 
 
         switch (Type.valueOf(type)) {
             case accommodation:
-                return "{\n" +
-                        "  \"@context\": \"http://schema.org/\",\n" +
-                        "  \"@type\": [\"LodgingBusiness\", \"Hotel\"],\n" +
-                        "  \"address\": {\n" +
-                        "    \"@type\": \"PostalAddress\"\n" +
-                        "  }\n" +
-                        "}";
+                return frameObject.get("accommodation").toString();
 
             default:
                 throw  new Exception("Unsupported request");
@@ -195,79 +248,15 @@ public class HttpServerVerticle extends AbstractVerticle {
 
     }
 
-    private String getConstructSparqlQuery(String type, String language, String id) throws Exception {
+    private String getConstructSparqlQuery(Toml toml, String type) throws Exception {
 
         switch (Type.valueOf(type)) {
             case accommodation:
-                return getAccommodationConstructQuery(language, id);
+                return toml.getString("accommodation.query");
 
             default:
                 throw new Exception("Unsupported request");
         }
 
-    }
-
-    //TODO check id and language for sql injection
-    private String getAccommodationConstructQuery(String language, String id) {
-
-        String uri = "<http://noi.example.org/ontology/odh#data/accommodation/"+id+">";
-        return "PREFIX schema: <http://schema.org/>\n" +
-                "CONSTRUCT {\n" +
-                "  ?h a ?cl ; schema:name ?nameStr ; schema:description ?desc ; schema:url ?url ; \n" +
-                "    schema:address ?a .\n" +
-                "   ?a a schema:PostalAddress ; schema:name ?aName ; schema:alternateName ?aAltName \n" +
-                "\n" +
-                "}\n" +
-                "WHERE {\n" +
-                "  ?h a schema:LodgingBusiness ; schema:name ?name ; schema:address ?a .\n" +
-                "  ?a a schema:PostalAddress .\n" +
-                "  FILTER (?h = "+uri+")"+
-                "  \n" +
-                "  # TODO: map it\n" +
-                "  OPTIONAL {\n" +
-                "    ?h schema:description ?desc . \n" +
-                "    FILTER (lang(?name) = lang(?desc))\n" +
-                "  }\n" +
-                "  \n" +
-                "  # TODO: map it\n" +
-                "  OPTIONAL {\n" +
-                "    ?h schema:url ?url .\n" +
-                "  }\n" +
-                "  \n" +
-                "  # TODO: map it\n" +
-                "  OPTIONAL {\n" +
-                "    ?a schema:name ?aName .\n" +
-                "    FILTER (lang(?name) = lang(?aName))\n" +
-                "  }\n" +
-                "\n" +
-                "  # TODO: map it\n" +
-                "  OPTIONAL {\n" +
-                "    ?a schema:alternateName ?aAltName .\n" +
-                "    FILTER (lang(?name) = lang(?aAltName))\n" +
-                "  }\n" +
-                "  \n" +
-                "  OPTIONAL {\n" +
-                "    ?h a schema:Hotel .\n" +
-                "    BIND (schema:Hotel AS ?c)\n" +
-                "  }\n" +
-                "  OPTIONAL {\n" +
-                "    ?h a schema:Campground .\n" +
-                "    BIND (schema:Campground AS ?c)\n" +
-                "  }\n" +
-                "  OPTIONAL {\n" +
-                "    ?h a schema:BedAndBreakfast .\n" +
-                "    BIND (schema:BedAndBreakfast AS ?c)\n" +
-                "  }\n" +
-                "  OPTIONAL {\n" +
-                "    ?h a schema:Hostel .\n" +
-                "    BIND (schema:Hostel AS ?c)\n" +
-                "  }\n" +
-                "  BIND (COALESCE(?c, schema:LodgingBusiness) AS ?cl)\n" +
-                "  \n" +
-                "  # Hiding language tags\n" +
-                "  BIND(str(?name) AS ?nameStr)\n" +
-                "  \n" +
-                "  FILTER (lang(?name) = COALESCE('"+language+"', 'en'))\n" +
-                "}\n" ;
     }
 }
